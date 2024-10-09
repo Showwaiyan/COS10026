4ddd80e4453fe5f68bb1be8bae2eb06d6b760176
showwaiyan <showwaiyan555@gmail.com>
2024-10-09 16:52:28 +0800

    Fixed mistakes for lab06

--- Lab/lab06/daysarray.php
+++ Lab/lab06/daysarray.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-   <?php
+    <h1>PHP Variables, Array and Operators</h1>
+    <?php
     $days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    echo "The days of the week in English are: <br>";
+    echo "<p>The days of the week in English are: </p>";
     for ($i = 0; $i < count($days); $i++) {
-        echo $days[$i] . "<br>";
+        echo $days[$i] . ($i + 1 == count($days) ? "." : ", ");
     }
     echo "<br>";
-    echo "The days of the week in French are: <br>";
+    echo "<p>The days of the week in French are: </p>";
     $days = array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
     for ($i = 0; $i < count($days); $i++) {
-        echo $days[$i] . "<br>";
+        echo $days[$i] . ($i + 1 == count($days) ? "." : ", ");
     }
-   ?>
+    ?>
+
+
 </body>
+
 </html>
